cf89b1091de54c05f292f56dce5477f6048baa47
dguil <32369954+dguil@users.noreply.github.com>
2017-10-13 12:01:51 -0600

    Revert "Revert "Clase Persona""
    This reverts commit 14122682f566d14e5bb8d120e71560ea3f7d7ca2.

---
+++ src/practice1/Persona.java
@@ -0,0 +1,12 @@
+package practice1;
+
+public class Persona {
+	String firstName;
+	String lastName;
+	String telephone;
+	
+	String getPersona() {
+		return this.firstName + ' ' + this.lastName + " Phone Number: "+this.telephone;
+	}
+
+}
