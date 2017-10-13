26963e8013e398811039f097d66f5576ec55546a
dguil <32369954+dguil@users.noreply.github.com>
2017-10-13 16:01:13 -0600

    Antes de Irme
    Cambios antes de irme

--- src/practice1/HolaMundo.java
+++ src/practice1/HolaMundo.java
@@ -1,10 +1,19 @@
 package practice1;
 
+
+
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
+		String firstName = "Douglas";
+		String lastName = "Guillen";
+		String telephone = "99885544";
+		
+		Persona p = new Persona(firstName,lastName,telephone);
+		
+		
+		System.out.println(p.getPersona());
 		
 	}
 }
--- src/practice1/Persona.java
+++ src/practice1/Persona.java
@@ -5,8 +5,41 @@ public class Persona {
 	String lastName;
 	String telephone;
 	
-	String getPersona() {
+	Persona familiares [];
+	
+	//Constructor por defecto.
+	Persona(){
+		
+	}
+	
+	Persona(String firstName, String lastName, String telephone){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.telephone = telephone;
+	}
+	
+	public String getPersona() {
 		return this.firstName + ' ' + this.lastName + " Phone Number: "+this.telephone;
 	}
+	
+	public void setFirstName(String firstName){
+		this.lastName = firstName;
+	}
+	
+	public void setTelephone(String telephone){
+		this.telephone = telephone;
+	}	
 
+	public String getFirstName(){
+		return this.firstName;
+	}
+	
+	public String getLastName(){
+		return this.lastName;
+	}
+	
+	public String getTelephone(){
+		return this.telephone;
+	}	
+	
 }
